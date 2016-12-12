636003dcbdd45418d89afcb643238106eaf103f8
senlinxuefeng <1107963848@qq.com>
2016-12-12 12:11:39 +0800

    完成了任务，需要合并分支到beta分支

--- app/src/main/java/com/yumingchuan/testgit/MainActivity.java
+++ app/src/main/java/com/yumingchuan/testgit/MainActivity.java
@@ -14,5 +14,9 @@ public class MainActivity extends AppCompatActivity {
         //第三次提交
 
         //第四次提交，修改本地分支dev为dev1,删除了本地,远程分支dev，建立本地分支的上传流、
+
+        //工作做了一般了，要修复bug这个时候用stash,
+
+        //回复stash保存的内容，修复了bug，继续工作，
     }
 }
